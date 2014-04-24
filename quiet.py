a72a4648278b274da8cafa98b9e0375e6d0703e6
Hiroshi Saito <hiroshi3110@gmail.com>
2014-04-24 22:20:48 +0900

    Change the app structure.

--- quiet.py
+++ quiet.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import webbrowser
-from Foundation import NSBundle
+from Foundation import NSBundle, NSObject
 import rumps
 import modules.google_calendar
 #rumps.debug_mode(True)  # turn on command line logging information for development - default is off
 
-def about(sender):
-    webbrowser.open("https://github.com/hiroshi/quiet/wiki")
+class Quiet(NSObject):
+    def init(self):
+        self = super(Quiet, self).init()
+        return self
+
+    def run(self):
+        app = rumps.App("Quiet", title='Q')
+        app.menu._menu.setDelegate_(self)
+        # modules
+        #modules.google_calendar.start(app)
+        self.google_calendar = modules.google_calendar.new()
+        self.google_calendar.start()
+        # Quiet menu items
+        app.menu.add(None) # separator
+        version = NSBundle.mainBundle().infoDictionary()['CFBundleShortVersionString']
+        def _about(sender):
+            webbrowser.open("https://github.com/hiroshi/quiet/wiki")
+        app.menu.add(rumps.MenuItem("quiet %s" % version, callback=_about))
+        app.run()
+
+    # NSMenuDelegate
+    # def menuWillOpen_(self, menu):
+    #     print menu
+    def menuDidClose_(self, menu):
+        print menu
+
+
 
 if __name__ == "__main__":
-    app = rumps.App("My Toolbar App", title='0')
-    app.title = "Q"
-    modules.google_calendar.start(app)
-    app.menu.add(None) # separator
-    version = NSBundle.mainBundle().infoDictionary()['CFBundleShortVersionString']
-    app.menu.add(rumps.MenuItem("quiet %s" % version, callback=about))
-    app.run()
+    Quiet.new().run()
+    # app = rumps.App("My Toolbar App", title='0')
+
+    # print app.menu._menu
+    # menu_delegate = MenuDelegate.alloc().init()
+    # app.menu._menu.setDelegate_(menu_delegate)
+    # print app.menu._menu.delegate()
+
+    # app.title = "Q"
+    # modules.google_calendar.start(app)
+    # app.menu.add(None) # separator
+    # version = NSBundle.mainBundle().infoDictionary()['CFBundleShortVersionString']
+    # app.menu.add(rumps.MenuItem("quiet %s" % version, callback=about))
+    # app.run()
